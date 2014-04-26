78183826475148b0eca80e70c73376ec052d420e
Mikkel Madsen <m@madsn.net>
2014-04-26 19:56:48 +0200

    Update background.js
    Cleanup

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-/*
- * Author: Mikkel Madsen - m@madsn.net
- * 2013-04-16
- */
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   console.log("Injecting JS");
